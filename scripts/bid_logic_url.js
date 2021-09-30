515c6ccdd8c8cb6fab5396b6b9176d538b15bb12
nikunj157 <nikunj157@gmail.com>
2021-09-30 00:26:46 -0700

    Create bid_logic_url.js

---
+++ scripts/bid_logic_url.js
@@ -0,0 +1,18 @@
+function generateBid(
+    interestGroup, auctionSignals, perBuyerSignals, trustedBiddingSignals,
+    browserSignals) {
+  int max_bid_index=0;
+  for (let i in interestGroup.ads) {
+  	if (interestGroup.ads[i].metadata.bid > interestGroup.ads[i].metadata.max_bid_index) max_bid_index = i;
+  }
+  const ad = interestGroup.ads[max_bid_index];
+  
+  return {'ad': 'anything',
+          'bid': ad.metadata.bid,
+          'render': ad.renderUrl};
+}
+
+function reportWin(
+  auctionSignals, perBuyerSignals, sellerSignals, browserSignals) {
+  sendReportTo("https://nikunja.users.x20web.corp.google.com/td/scripts/fakeEndPoint");
+}
