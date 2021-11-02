bf361f2ff08ae5042c5ffda06b63dba23e4b8391
nikunj157 <nikunj157@gmail.com>
2021-11-02 00:37:50 -0700

    Update JoinIG page for non cache support, skip symlinks

---
+++ scripts_no_cache/bid_logic_url.js
@@ -0,0 +1,18 @@
+function generateBid(
+    interestGroup, auctionSignals, perBuyerSignals, trustedBiddingSignals,
+    browserSignals) {
+  let max_bid_index = 0;
+  for (let i = 0; i < interestGroup.ads.length; i++){
+  	if (interestGroup.ads[i].metadata.bid > interestGroup.ads[max_bid_index].metadata.bid) max_bid_index = i;
+  }
+  const ad = interestGroup.ads[max_bid_index];
+  
+  return {'ad': 'anything',
+          'bid': ad.metadata.bid,
+          'render': ad.renderUrl};
+}
+
+function reportWin(
+  auctionSignals, perBuyerSignals, sellerSignals, browserSignals) {
+  sendReportTo("https://td-1ha7.onrender.com/scripts/fakeEndPoint");
+}
